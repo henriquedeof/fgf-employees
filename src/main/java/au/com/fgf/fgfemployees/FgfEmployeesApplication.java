f2f002ed833bff005f0d44da61081e48057bdb27
henriquedeof <henriquedeof@gmail.com>
2021-07-09 11:47:43 +1000

    First commit.

---
+++ src/main/java/au/com/fgf/fgfemployees/FgfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package au.com.fgf.fgfemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgfEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FgfEmployeesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/au/com/fgf/fgfemployees/FgfEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.fgf.fgfemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgfEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
